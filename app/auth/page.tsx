cc319de1a9e672e95e6414d94f881a133206b4f3
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-06-26 19:36:57 +0530

    sign up page ui test

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image"
+import { Input } from "@/components/ui/input"
 
 import hero from '@/public/img/maninflowers.png'
+import { Button } from "@/components/ui/button"
 const page = () => {
     return (
         <>
-            <Image fill src={hero} alt="" />
-            Sign up
+            <Image className="brightness-50" fill src={hero} alt="" />
+            <div className="absolute bottom-32">
+                Welcome to omo omo
+                <p>SIGN UP NOW</p>
+                <Input className="bg-white" placeholder="Email address" />
+                <Button>SIGN UP</Button>
+
+            </div>
+
         </>
     )
 }
