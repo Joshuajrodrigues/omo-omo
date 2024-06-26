1be948215986d696c644b4aa943ee163379d60b3
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-06-26 22:17:14 +0530

    mobile

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -7,17 +7,18 @@ import { TypographyH1 } from "@/components/ui/typographyH1"
 import { TypographyH2 } from "@/components/ui/typographyH2"
 const page = () => {
     return (
-        <>
-            <Image className="brightness-50" fill src={hero} alt="" />
-            <div className="absolute bottom-28 left-28 flex flex-col gap-y-7 w-[650px] ">
+        <div className="relative">
+            <div className="relative top-0 bottom-0 min-h-screen">
+                <Image className="brightness-50" fill src={hero} alt="" placeholder="blur" />
+            </div>
+            <div className="absolute bottom-24 left-8 md:bottom-28 md:left-28 flex flex-col gap-y-7  w-[350px] md:w-[60%] ">
                 <TypographyH1 className="text-white">WELCOME TO <br /> OMO OMO</TypographyH1>
-
                 <TypographyH2 className=" text-white">SIGN UP NOW</TypographyH2>
-                <Input className="bg-white w-[60%]" placeholder="Email address" />
+                <Input className="bg-white w-full md:w-[60%]" placeholder="Email address" />
                 <Button className=" w-24">SIGN UP</Button>
             </div>
 
-        </>
+        </div>
     )
 }
 
