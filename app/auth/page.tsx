dd0f517ffa87b850defcadbfc8b664f1af444c34
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-06-27 22:17:04 +0530

    text color fix

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
             <div className="absolute p-5 md:p-24 w-full bottom-32 flex flex-col gap-y-6 text-white ">
                 <TypographyH1 className="">WELCOME TO <br /> OMO OMO</TypographyH1>
                 <TypographyH2 className="  text-xl md:text-2xl">SIGN UP NOW</TypographyH2>
-                <Input className="bg-white w-[80%] md:w-[40%]" placeholder="Email address" />
+                <Input className="bg-white text-black w-[80%] md:w-[40%]" placeholder="Email address" />
                 <Button className=" w-24 font-semibold">SIGN UP</Button>
                 <p className=" max-w-[550px] ">By clicking 'Sign Up' you are indicating that you have read and agree to the Terms of Service and Privacy</p>
             </div>
