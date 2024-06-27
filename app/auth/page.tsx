1cb37150ef2ad2bac9277dea8e984838743d9aa9
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-06-27 21:59:33 +0530

    sign up page mock done

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -12,11 +12,12 @@ const page = () => {
                 <Image className=" brightness-50" fill src={hero} alt="" placeholder="blur" />
             </div>
 
-            <div className="absolute p-5 md:p-24 w-full bottom-32 flex flex-col gap-y-6 ">
-                <TypographyH1 className="text-white">WELCOME TO <br /> OMO OMO</TypographyH1>
-                <TypographyH2 className=" text-white">SIGN UP NOW</TypographyH2>
+            <div className="absolute p-5 md:p-24 w-full bottom-32 flex flex-col gap-y-6 text-white ">
+                <TypographyH1 className="">WELCOME TO <br /> OMO OMO</TypographyH1>
+                <TypographyH2 className="  text-xl md:text-2xl">SIGN UP NOW</TypographyH2>
                 <Input className="bg-white w-[80%] md:w-[40%]" placeholder="Email address" />
-                <Button className=" w-24">SIGN UP</Button>
+                <Button className=" w-24 font-semibold">SIGN UP</Button>
+                <p className=" max-w-[550px] ">By clicking 'Sign Up' you are indicating that you have read and agree to the Terms of Service and Privacy</p>
             </div>
 
 
--- components/fonts.ts
+++ components/fonts.ts
@@ -1,3 +1,3 @@
 import { Inter, DM_Sans } from "next/font/google";
 
-export const dmSans = DM_Sans({ weight: ["700"], subsets: ["latin"] })
+export const dmSans = DM_Sans({ weight: ["700", "200"], subsets: ["latin"] })
