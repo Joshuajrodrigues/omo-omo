894b720dc8a0949ea5958cb6b7f4e06ba1a50806
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-08-03 18:55:30 +0530

    triggers added

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import fs from "fs"
+import { sql } from 'drizzle-orm';
 config({ path: '.env' });
 
 const client = postgres(process.env.DATABASE_URL!, {
@@ -9,5 +9,28 @@ const client = postgres(process.env.DATABASE_URL!, {
 });
 export const db = drizzle(client);
 
+async function createTrigger() {
 
+    await db.execute(sql`
+        
+         -- Ensure the "normie" role exists
+            INSERT INTO roles (name, isBaseRole) VALUES ('normie', TRUE) ON CONFLICT (name) DO NOTHING;
+        -- Create a function to insert the default role
+            CREATE OR REPLACE FUNCTION assign_default_role() RETURNS trigger AS $$
+            BEGIN
+                INSERT INTO userRoles (userId, roleId)
+                VALUES (NEW.id, (SELECT id FROM roles WHERE name = 'normie'));
+    
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+         -- Create the trigger to call the function on user insertion
+            CREATE TRIGGER after_user_insert
+            AFTER INSERT ON users
+            FOR EACH ROW
+            EXECUTE FUNCTION assign_default_role();
+    
+        `)
+}
 
+createTrigger().catch(console.error);
