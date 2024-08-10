39c78851237c0f04072b7dd6d275edaaa8eecd2a
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-08-10 18:43:50 +0530

    semi responsive for featured content

--- app/page.tsx
+++ app/page.tsx
@@ -22,18 +22,20 @@ export default async function Home() {
         }
     ]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
         <section className={'w-full relative'}>
 
-            <div className={" grid grid-cols-3 gap-1 -ml-24 -mr-24"}>
+            <div className={" grid grid-cols-3 gap-1 "}>
             {
                 images.map((image) => (
                     <Image className={"w-full h-full object-cover "} key={image.id} alt={""} src={image.img} />
                 ))
             }
             </div>
-            <TypographyH2 className={'text-6xl absolute top-0 -mt-20 '}>YOUR INDIE MOVIE <br/> HAVEN</TypographyH2>
-            <TypographyH2 className={'text-6xl text-right -mt-20'}>TRUELY FOR THE <br/> CREATOR</TypographyH2>
+
+            <TypographyH2 className={'lg:text-6xl md:text-4xl -mt-12  px-10 sm:px-20 left-0  absolute top-0'}>YOUR INDIE MOVIE <br/> HAVEN</TypographyH2>
+            <TypographyH2 className={'lg:text-6xl md:text-4xl text-right -mt-12  px-10 sm:px-20 md:-mt-16'}>TRUELY FOR THE <br/> CREATOR</TypographyH2>
+
         </section>
 
       {
